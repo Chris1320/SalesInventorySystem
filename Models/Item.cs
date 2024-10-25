b7e7841448ee41508396076aae6a69cbdb039ea9
chris1320 <chris1320is@protonmail.com>
2024-10-26 01:35:32 +0800

    feat: add entity models
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

---
+++ Models/Item.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace SalesInventorySystem_WAM1.Models
+{
+    public class Item
+    {
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi
+        // ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi  ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang ako ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi ako nalang kasi AKO  NALANG KASIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public double UnitPrice { get; set; }
+        public int Stock { get; set; }
+
+        public DateTime DateAdded { get; }
+        public DateTime DateModified { get; set; }
+    };
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SalesInventorySystem_WAM1.Models
+{
+    public class Transaction
+    {
+        public DateTime Timestamp { get; }
+
+        public int ItemId { get; }
+        public int Quantity { get; }
+
+        public string Status { get; }
+        public string Notes { get; }
+    }
+}
